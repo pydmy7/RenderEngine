5585c33b7f425a360af7cb2c554dd8208030da33
wangyao <1822364545@qq.com>
2024-10-05 17:28:52 +0800

    spdlog

--- renderengine/src/main.cpp
+++ renderengine/src/main.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 #include <QApplication>
@@ -8,10 +9,16 @@
 #include "renderengine/renderengine.hpp"
 
 auto initLog = []() -> int {
-    const auto file_logger{spdlog::basic_logger_mt(
-        "master",
-        (BINARY_DIR / (std::string(PROJECT_NAME) + ".log")).string())};
-    spdlog::set_default_logger(file_logger);
+    const auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+        (BINARY_DIR / (std::string(PROJECT_NAME) + ".log")).string(), true);
+    const auto console_sink =
+        std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+
+    auto logger =
+        std::make_shared<spdlog::logger>("master", sinks.begin(), sinks.end());
+    spdlog::set_default_logger(logger);
+
     return 0;
 }();
 
